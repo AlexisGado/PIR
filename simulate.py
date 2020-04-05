aed344831375266cb8dbaa5393b1b89d5ba9382d
remi-simon <60354382+remi-simon@users.noreply.github.com>
2020-04-05 16:03:11 +0200

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -23,6 +23,69 @@ class Manager():
         self.prices = np.zeros(48)
         
     
+        ## Données
 
-    
+    L_pv=[0]*48  #photovoltaic production per slot
+    L_dem=[0]*48  #industrial needs per slot
+    Price=[0]*48 #price reference per slot
+    Planning=[[16,36] for i in range(4)] #departure and arrival of the 4 EV
+
+
+    ##Compute the energy balance on a slot
+    def energy_balance(self, time):
+
+        total_load = 0
+        demand = 0
+        supply = 0
+
+        for name, player in self.players.items():
+
+            player.compute_load(time)
+            load = player.load[time]
+
+            if load >= 0: #if the player needs energy
+                demand += load
+            else:         #if the player supply energy
+                supply -= load
+            total_load += load   #mesure the balance
+
+        return total_load, demand, supply
+
+
+    ## Compute the bill of each players 
+    def compute_bills(self, time, load, demand, supply):
+
+        purchase = self.prices[time]*demand*self.dt  #sum of purchases on the grid
+        sale = self.prices[time]*supply*self.dt  #sum of sales on the grid
+
+        for name, player in self.players.items():
+
+            if demand != 0:  
+                player.information["grid_buy_price"][time+1] = purchase / (demand*self.dt) #purchasing price per unit of energy
+            if supply != 0:
+                player.information["grid_sell_price"][time+1] = sale / (supply*self.dt) #selling price per unit of energy
+
+            load = player.load[time]
+            if load == 0:
+                continue
+
+            if load >= 0:  #if the player needs energy
+                cost = purchase * load / demand   #the player pays its proportion of purchase
+                player.bill[time] += cost
+                player.information["my_buy_price"][time+1] = purchase / (demand*self.dt)
+            else:   #if the player supply energy
+                revenue = sale * load / supply
+                player.bill[time] += revenue  #the player earns its proportion of supply
+                player.information["my_sell_price"][time+1] = sale / (supply*self.dt)
+
+    ##Playing one party 
+
+    def play(self):
+        #non flexible varibale to introduce here
+        for t in range(self.horizon):
+            load, demand, supply = self.energy_balance(t)
+            self.compute_bills(t, load, demand, supply)
+
+
+
 
