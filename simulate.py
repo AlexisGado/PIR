c9d0c68d45ef2c2ea5addaa6f980e54adb5356da
remi-simon <60354382+remi-simon@users.noreply.github.com>
2020-04-14 19:03:46 +0200

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -30,14 +30,13 @@ class Manager():
             "industrial_site": IndustrialConsumer(0,0)}  #To be modified
             
         self.prices = {"internal" : prices[0, :], "external_purchase" : prices[1, :], "external_sale" : prices[2, :]}
-
+        self.imbalance=[]
     
     
 
-##Compute the energy balance on a slot
+           ##Compute the energy balance on a slot
     def energy_balance(self, time):
 
-        total_load = 0
         demand = 0
         supply = 0
 
@@ -50,14 +49,13 @@ class Manager():
                 demand += load
             else:         #if the player supply energy
                 supply -= load
-            total_load += load   #measure the balance
 
-        return total_load, demand, supply
+        return  demand, supply
 
 
-## Compute the bill of each players 
-    def compute_bills(self, time, load, demand, supply):
-    
+    ## Compute the bill of each players 
+    def compute_bills(self, time, demand, supply):
+        total_load=demand-supply    #total load of the grid
         internal_exchange=min(demand,supply)  #what is going to be exchange on the grid
         external_exchange=abs(total_load)   #the quantity of energy in surplus on the grid
         internal_price=self.prices["internal"][time]
@@ -65,6 +63,11 @@ class Manager():
         external_purchasing_price=self.prices["external_purchase"][time]
 
         if total_load>=0:  #if there is not enough energy on the grid
+            
+            proportion_internal_demand=internal_exchange/demand
+            proportion_internal_supply=1
+            
+            self.imbalance.append({"proportion_internal_demand": proportion_internal_demand,"proportion_internal_supply": proportion_internal_supply} )
 
             for name, player in self.players.items():
 
@@ -72,10 +75,9 @@ class Manager():
 
                 if load>0: #if the player needs energy
 
-                    cost= (internal_price*(internal_exchange/demand) + external_purchasing_price*(external_exchange/demand))*load*dt
+                    cost= (internal_price*(proportion_internal_demand) + external_purchasing_price*(1-proportion_internal_demand))*load*dt
                             #the players pays in proportion on and off the grid for his demand
                     player.bill[time] += cost
-                    player.information["proportion_internal_demand"][time] = internal_exchange/demand
 
                 elif load<0: #if the player supply energy
 
@@ -84,24 +86,25 @@ class Manager():
                     player.information["proportion_internal_supply"][time]=1
 
         else :   #if the offer is too consequent on the grid
-
+            
+            proportion_internal_demand=1
+            proportion_internal_supply=internal_exchange/demand
+            self.imbalance.append({"proportion_internal_demand": proportion_internal_demand,"proportion_internal_supply": proportion_internal_supply} )
+            
             for name, player in self.players.items():
-
+                
                 load=player.load[time]
 
                 if load>0: #if the player needs energy
 
                     cost=internal_price*load*dt  #there is enough energy produced on the grid
                     player.bill[time] += cost
-                    player.information["proportion_internal_demand"][time] = 1
 
                 elif load<0:  #if the player supply energy
 
-                    revenue= (internal_price*(internal_exchange/supply) + external_selling_price*(external_exchange/supply))*load*dt
+                    revenue= (internal_price*(proportion_internal_supply) + external_selling_price*(1-proportion_internal_supply))*load*dt
                             #the players pays in proportion of his supply
                     player.bill[time] += revenue
-                    player.information["proportion_internal_supply"][time]=internal_exchange/supply
-                    
     
 ## Draw a scenario for the day
     
