521dfa5f52b9840954efb2ef3bdf0b3e4b9ee88b
remi-simon <60354382+remi-simon@users.noreply.github.com>
2020-04-17 17:38:02 +0200

    Update simulate_test.py

--- simulate_test.py
+++ simulate_test.py
@@ -1,4 +1,10 @@
+import numpy as np
 from simulate import Manager
 
 M=Manager()
-M.play()
+M.simulate(100)
+
+loads=np.load("load simulation.npy")
+bills=np.load("bill simulation.npy")
+prices=np.load("price simulation.npy")
+battery=np.load("battery stock simulation.npy")
